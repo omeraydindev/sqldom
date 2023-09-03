300a8413eb137bbfedb8e66ae387d950f5115b60
MikeAndrson <45513948+MikeAndrson@users.noreply.github.com>
2023-09-03 18:31:35 +0300

    feat: Support ORDER BY & LIMIT clauses

--- sqldom.js
+++ sqldom.js
@@ -60,13 +60,13 @@ function execSql(sql, options) {
     }
 
     function evalSelectStatement(ast) {
-        const {where, columns} = ast;
+        const {columns} = ast;
         const tables = getTablesFromAst(ast);
         const usedTables = tables?.length > 0;
 
         let elements;
         if (usedTables) {
-            elements = selectElements(tables, where);
+            elements = selectElements(ast);
         }
 
         if (columns.some(c => c.expr.column === '*')) {
@@ -91,9 +91,8 @@ function execSql(sql, options) {
     }
 
     function evalUpdateStatement(ast) {
-        const {set, where} = ast;
-        const tables = getTablesFromAst(ast);
-        const elements = selectElements(tables, where);
+        const {set} = ast;
+        const elements = selectElements(ast);
 
         elements.forEach(element => {
             set.forEach(({column, value}) => {
@@ -105,18 +104,14 @@ function execSql(sql, options) {
     }
 
     function evalDeleteStatement(ast) {
-        const {where} = ast;
-        const tables = getTablesFromAst(ast);
-        const elements = selectElements(tables, where);
+        const elements = selectElements(ast);
 
         elements.forEach(element => element.remove());
 
         return elements;
     }
 
-    /**
-     * @returns {Array<{table: string}>}
-     */
+    /** @returns {Array<{table: string}>} */
     function getTablesFromAst(ast) {
         if (ast.type === 'select') {
             return ast.from;
@@ -127,23 +122,69 @@ function execSql(sql, options) {
         throw new Error('Unsupported query type: ' + ast.type);
     }
 
-    /** @returns {string} */
-    function buildCssSelector(tables) {
+    /** @returns {Array<HTMLElement>} */
+    function selectElements(ast) {
+        const {where, limit, orderby} = ast;
+        const tables = getTablesFromAst(ast);
+
         // If any of the tables is "dom", select all elements
-        return tables.some(t => /dom/i.test(t.table))
+        const selector = tables.some(t => /dom/i.test(t.table))
             ? '*'
             : tables.map(t => t.table).join(',');
-    }
-
-    function selectElements(tables, where) {
-        const selector = buildCssSelector(tables);
 
         let elements = [...document.querySelectorAll(selector)];
+
         if (where) {
             elements = elements.filter(element => evalWhere(element, where));
         }
 
+        if (orderby) {
+            elements = applyOrderBy(elements, orderby);
+        }
+
+        if (limit) {
+            elements = applyLimit(elements, limit);
+        }
+
         return elements;
+    }
+
+    function applyOrderBy(elements, orderby) {
+        orderby.forEach(({expr, type}) => {
+            elements.sort((e1, e2) => {
+                const operand1 = evalOperand(e1, expr);
+                const operand2 = evalOperand(e2, expr);
+
+                if (operand1 == null) {
+                    return type === 'DESC' ? 1 : -1;
+                }
+                if (operand2 == null) {
+                    return type === 'DESC' ? -1 : 1;
+                }
+
+                return type === 'DESC'
+                    ? operand2.localeCompare(operand1)
+                    : operand1.localeCompare(operand2);
+            });
+        });
+
+        return elements;
+    }
+
+    function applyLimit(array, limit) {
+        const {seperator, value: [arg1, arg2]} = limit;
+
+        if ((arg1?.value && arg1.value < 0) || (arg2?.value && arg2.value < 0)) {
+            throw new Error("LIMIT/OFFSET clauses can't contain negative numbers");
+        }
+
+        if (seperator === '') {
+            return array.slice(0, arg1.value);
+        } else if (seperator === ',') {
+            return array.slice(arg1.value, arg1.value + arg2.value);
+        } else if (seperator === 'offset') {
+            return array.slice(arg2.value, arg1.value + arg2.value);
+        }
     }
 
     function evalOperand(element, operand) {
